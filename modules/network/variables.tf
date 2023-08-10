9fe749865c280d7c61ab412e30f3990c248bb541
Zack Siri <me@zacksiri.dev>
2023-08-10 10:21:35 +0700

    update network to infer availability zones from region

--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -14,8 +14,7 @@ variable "public_subnet_cidrs" {
   default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
 
-variable "availability_zones" {
-  type        = list(string)
-  description = "Availability Zones"
-  default     = ["us-west-2a", "us-west-2b", "us-west-2c"]
+variable "region" {
+  type        = string
+  description = "Region for availability zones"
 }
