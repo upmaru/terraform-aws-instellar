d530b7b6169d6c127004f4a0b8b2c9dde20f5e66
Zack Siri <me@zacksiri.dev>
2024-03-24 15:15:08 +0700

    Add description for balancer rules

--- modules/balancer/firewall.tf
+++ modules/balancer/firewall.tf
@@ -21,6 +21,7 @@ resource "aws_vpc_security_group_egress_rule" "allow_outgoing" {
 
 resource "aws_vpc_security_group_ingress_rule" "http_v4" {
   security_group_id = aws_security_group.this.id
+  description       = "Enable public http v4 to load balancer"
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 80
   to_port           = 80
@@ -33,6 +34,7 @@ resource "aws_vpc_security_group_ingress_rule" "http_v4" {
 
 resource "aws_vpc_security_group_ingress_rule" "http_v6" {
   security_group_id = aws_security_group.this.id
+  description       = "Enable public http v6 to load balancer"
   cidr_ipv6         = "::/0"
   from_port         = 80
   to_port           = 80
@@ -45,6 +47,7 @@ resource "aws_vpc_security_group_ingress_rule" "http_v6" {
 
 resource "aws_vpc_security_group_ingress_rule" "https_v4" {
   security_group_id = aws_security_group.this.id
+  description       = "Enable public https v4 to load balancer"
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 443
   to_port           = 443
@@ -57,6 +60,7 @@ resource "aws_vpc_security_group_ingress_rule" "https_v4" {
 
 resource "aws_vpc_security_group_ingress_rule" "https_v6" {
   security_group_id = aws_security_group.this.id
+  description       = "Enable public https v6 to load balancer"
   cidr_ipv6         = "::/0"
   from_port         = 443
   to_port           = 443
@@ -69,6 +73,7 @@ resource "aws_vpc_security_group_ingress_rule" "https_v6" {
 
 resource "aws_vpc_security_group_ingress_rule" "lxd_v4" {
   security_group_id = aws_security_group.this.id
+  description       = "Enable public lxd v4 to load balancer"
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 8443
   to_port           = 8443
@@ -81,6 +86,7 @@ resource "aws_vpc_security_group_ingress_rule" "lxd_v4" {
 
 resource "aws_vpc_security_group_ingress_rule" "lxd_v6" {
   security_group_id = aws_security_group.this.id
+  description       = "Enable public lxd v6 to load balancer"
   cidr_ipv6         = "::/0"
   from_port         = 8443
   to_port           = 8443
@@ -93,6 +99,7 @@ resource "aws_vpc_security_group_ingress_rule" "lxd_v6" {
 
 resource "aws_vpc_security_group_ingress_rule" "uplink_v4" {
   security_group_id = aws_security_group.this.id
+  description       = "Enable public uplink v4 to load balancer"
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 49152
   to_port           = 49152
@@ -105,6 +112,7 @@ resource "aws_vpc_security_group_ingress_rule" "uplink_v4" {
 
 resource "aws_vpc_security_group_ingress_rule" "uplink_v6" {
   security_group_id = aws_security_group.this.id
+  description       = "Enable public uplink v6 to load balancer"
   cidr_ipv6         = "::/0"
   from_port         = 49152
   to_port           = 49152
@@ -116,8 +124,8 @@ resource "aws_vpc_security_group_ingress_rule" "uplink_v6" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "nodes_http" {
-  security_group_id = var.nodes_security_group_id
-
+  security_group_id            = var.nodes_security_group_id
+  description                  = "Enable load balancer traffic for http"
   referenced_security_group_id = aws_security_group.this.id
   from_port                    = 80
   to_port                      = 80
@@ -129,8 +137,8 @@ resource "aws_vpc_security_group_ingress_rule" "nodes_http" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "nodes_https" {
-  security_group_id = var.nodes_security_group_id
-
+  security_group_id            = var.nodes_security_group_id
+  description                  = "Enable load balancer traffic for https"
   referenced_security_group_id = aws_security_group.this.id
   from_port                    = 443
   to_port                      = 443
@@ -142,8 +150,8 @@ resource "aws_vpc_security_group_ingress_rule" "nodes_https" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "nodes_lxd" {
-  security_group_id = var.nodes_security_group_id
-
+  security_group_id            = var.nodes_security_group_id
+  description                  = "Enable load balancer traffic for lxd"
   referenced_security_group_id = aws_security_group.this.id
   from_port                    = 8443
   to_port                      = 8443
@@ -155,8 +163,8 @@ resource "aws_vpc_security_group_ingress_rule" "nodes_lxd" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "nodes_uplink" {
-  security_group_id = var.nodes_security_group_id
-
+  security_group_id            = var.nodes_security_group_id
+  description                  = "Enable load balancer traffic for uplink"
   referenced_security_group_id = aws_security_group.this.id
   from_port                    = 49152
   to_port                      = 49152
