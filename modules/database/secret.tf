1357bf1556824abc06b302d6b52222ea3fc686c9
Zack Siri <me@zacksiri.dev>
2024-03-27 13:53:54 +0700

    Choose db instance for referencing secret

--- modules/database/secret.tf
+++ modules/database/secret.tf
@@ -9,7 +9,7 @@ data "aws_iam_policy_document" "this" {
     ]
 
     resources = [
-      aws_secretsmanager_secret.this.master_user_secret[0].secret_arn
+      aws_db_instance.this.master_user_secret[0].secret_arn
     ]
   }
 }
