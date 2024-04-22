4e065c43ff2f5fdd806ac2b7a784936f92a867a5
Zack Siri <me@zacksiri.dev>
2024-04-22 16:19:46 +0700

    Setup global accelerator

---
+++ modules/global-accelerator/variables.tf
@@ -0,0 +1,14 @@
+variable "blueprint" {
+  description = "Identifier of the blueprint"
+  type        = string
+}
+
+variable "identifier" {
+  description = "Identiifer for global accelerator"
+  type        = string
+}
+
+variable "region" {
+  description = "Region for endpoint group"
+  type        = string
+}
