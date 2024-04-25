b17c862d54c600aa2b0e5e51138bd8e68928542d
Zack Siri <me@zacksiri.dev>
2024-04-25 15:58:10 +0700

    Output global accelerator as balancer

---
+++ modules/global-accelerator/outputs.tf
@@ -0,0 +1,8 @@
+output "address" {
+  description = "Address of the Global Accelerator"
+  value = aws_globalaccelerator_accelerator.this.dual_stack_dns_name
+}
+
+output "name" {
+  value = var.identifier
+}
